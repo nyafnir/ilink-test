63c29de11a89b31ee073bddfd679682e857e5c83
nyafnir <leonid.b4@yandex.ru>
2022-01-20 21:59:55 +0700

    Подключение .env, базы данных (postgres) и сущностей

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GroupsModule } from './groups/groups.module';
+import { UsersModule } from './users/users.module';
+import { config as loadEnvFile } from 'dotenv';
+
+loadEnvFile({ path: '../.env' });
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'postgres' | 'mongodb',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
+    }),
+    GroupsModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- app/src/main.ts
+++ app/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config as loadEnvFile } from 'dotenv';
+
+loadEnvFile({ path: '../.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
