9ed1184be0b37702af2bded7ea8b37b222fa0675
nyafnir <leonid.b4@yandex.ru>
2022-01-22 09:23:11 +0700

    Замена PostgreSQL на MongoDB

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -1,33 +1,33 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { GroupsModule } from './groups/groups.module';
 import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
 import { FriendsModule } from './friends/friends.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 
 const isDevelopmentMode = process.env.NODE_ENV !== 'prod';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: process.env.DATABASE_TYPE as 'postgres' | 'mongodb',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: isDevelopmentMode,
-      logging: isDevelopmentMode,
-    }),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}` +
+        `@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}` +
+        `/${process.env.DATABASE_NAME}`,
+      {
+        autoIndex: true,
+        authSource: 'admin',
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    ),
     GraphQLModule.forRoot({
       debug: isDevelopmentMode,
       playground: isDevelopmentMode,
       autoSchemaFile: true,
       sortSchema: true,
     }),
-    GroupsModule,
     UsersModule,
+    GroupsModule,
     FriendsModule,
   ],
 })
